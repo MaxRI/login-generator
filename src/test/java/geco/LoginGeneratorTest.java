d862288868e772557b51adc2cc70e80e566d8a12
maxime_r <max3129@hotmail.fr>
2017-11-21 11:41:28 +0100

    #2 added loginGeneratorTest, added first test

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,39 @@
+package geco;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by rlm0843a on 21/11/2017.
+ */
+public class LoginGeneratorTest {
+
+    LoginService loginService = new LoginService(new String[] {"JROL", "BPER",
+            "CGUR", "JDUP", "JRAL", "JRAL1"});
+    LoginGenerator loginGenerator = new LoginGenerator(loginService);
+
+    @Test
+    public void TestGenPaulDurand(){
+        assertEquals(
+                loginGenerator.generateLoginForNomAndPrenom(
+                        "Durand", "Paul")
+                , "PDUR");
+    }
+
+    /*@Test
+    public void TestGenJeanRolling(){
+        assertEquals(
+                loginGenerator.generateLoginForNomAndPrenom(
+                        "Rolling", "Jean")
+                , "JROL1");
+    }
+
+    @Test
+    public void TestGenPaulDuurand(){
+        assertEquals(
+                loginGenerator.generateLoginForNomAndPrenom(
+                        "Dùrand", "Paul")
+                , "PDUR");
+    }*/
+}
